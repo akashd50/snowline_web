2d1926543a35940a8a97057d5afd032ad87ae5b4
akashd50 <akash.as19@gmail.com>
2021-11-18 20:17:24 -0600

    Update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { AppMenuComponent } from './app-menu/app-menu.component';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSliderModule } from '@angular/material/slider';
+import { MapComponent } from './map/map.component';
+import { HeaderBarComponent } from './header-bar/header-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppMenuComponent,
+    MapComponent,
+    HeaderBarComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatSliderModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
