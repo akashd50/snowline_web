70473238d37e3847e70696355cbe1c705e05713e
akashd50 <akash.as19@gmail.com>
2021-12-16 00:07:18 -0600

    Some cleanup

--- src/app/route-list-item/route-list-item.component.ts
+++ src/app/route-list-item/route-list-item.component.ts
@@ -1,22 +1,22 @@
-import { Component, OnInit } from '@angular/core';
-import { HttpRequestService } from '../services/http-request-service'
-import { newStopsRequest } from "../services/request";
+import {Component, OnInit} from '@angular/core';
+import {HttpRequestService} from '../services/http-request-service'
+import {createNewRequest, RequestType, StopsRequest} from "../services/request";
 
 @Component({
-  selector: 'app-route-list-item',
-  templateUrl: './route-list-item.component.html',
-  styleUrls: ['./route-list-item.component.css'],
+    selector: 'app-route-list-item',
+    templateUrl: './route-list-item.component.html',
+    styleUrls: ['./route-list-item.component.css'],
 })
 export class RouteListItemComponent implements OnInit {
 
-  constructor(private readonly requestService : HttpRequestService) { }
+    constructor(private readonly requestService : HttpRequestService) { }
 
-  ngOnInit(): void {
-    const request = newStopsRequest();
-    request.stopId = "10064";
-    this.requestService.getResponse(request).subscribe(response => {
-        console.log(response);
-    });
-  }
+    ngOnInit(): void {
+        const request = createNewRequest<StopsRequest>(RequestType.Stops);
+        request.stopId = "10064";
+        this.requestService.getResponse(request).subscribe(response => {
+            console.log(response);
+        });
+    }
 
 }
--- src/app/services/request.ts
+++ src/app/services/request.ts
@@ -1,61 +1,61 @@
 import {API_URL, STOPS} from "./consts";
 
 export enum RequestType {
-  Stops,
-  None
+    Stops,
+    None
 }
 
 export const paramsMap = {
-  [RequestType.Stops]: ["usage", "distance", "x", "y", "walking", "street", "route", "variant", "lat", "lon"]
+    [RequestType.Stops]: ["usage", "distance", "x", "y", "walking", "street", "route", "variant", "lat", "lon"]
 }
 
 export const requestTypeToUrl = {
-  [RequestType.Stops]: STOPS
+    [RequestType.Stops]: STOPS
 }
 
 export interface Request {
-  id?: string;
-  requestType: RequestType;
-  usage?: string;
+    id?: string;
+    requestType: RequestType;
+    usage?: string;
 }
 
 export interface StopsRequest extends Request {
-  stopId?:string; // Find information about this stop
-  street?: string;
-  route?: string;
-  variant?: string;
-  x?: string;
-  y?: string;
-  lat?: string;
-  lon?: string;
-  distance?: string;
-  walking?: boolean;
+    stopId?:string; // Find information about this stop
+    street?: string;
+    route?: string;
+    variant?: string;
+    x?: string;
+    y?: string;
+    lat?: string;
+    lon?: string;
+    distance?: string;
+    walking?: boolean;
 }
 
-export function newStopsRequest(): StopsRequest {
-  return {
-    requestType: RequestType.Stops,
-    usage: "long"
-  } as StopsRequest;
+export function createNewRequest<Type>(requestType: RequestType): Type {
+    return {
+        requestType: requestType,
+        usage: "long"
+    } as unknown as Type;
 }
 
 export function getUrl(request: Request): string {
-  if (request.requestType == RequestType.Stops) {
-    const stopRequest = request as StopsRequest;
-    const params = paramsMap[stopRequest.requestType];
-    let url = API_URL + requestTypeToUrl[stopRequest.requestType] + "/";
-    if (stopRequest.stopId) {
-      url += stopRequest.stopId;
-    }
-    url += ".json";
-    url += "?api-key=OrN9z8x8KwWXb5o39hoD";
+    if (request.requestType == RequestType.Stops) {
+        const stopRequest = request as StopsRequest;
+        const params = paramsMap[stopRequest.requestType];
+        let url = API_URL + requestTypeToUrl[stopRequest.requestType] + "/";
+        if (stopRequest.stopId) {
+            url += stopRequest.stopId;
+        }
+        url += ".json";
+        url += "?api-key=OrN9z8x8KwWXb5o39hoD";
 
-    params.forEach(p => {
-      if (stopRequest[p]) {
-        url += "&";
-        url += p + "=" + stopRequest[p];
-      }
-    });
-    return url;
-  }
+        params.forEach(p => {
+            if (stopRequest[p]) {
+                url += "&";
+                url += p + "=" + stopRequest[p];
+            }
+        });
+        return url;
+    }
 }
